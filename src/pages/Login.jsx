088ce5663e13a6d574e7a0a7e3852aca040a1d00
[Neeraj kushwaha] <[workwithneeraj.01@gmail.com]>
2025-11-17 21:12:31 +0530

    blah blah

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -117,7 +117,7 @@ const Login = () => {
               <div className="text-center">
                 <a href="#" className="text-blue-600 hover:text-blue-700 font-medium text-sm underline">
                   Forget Password?
-                </a>
+                </a>        
               </div>
             </form>
           </div>
